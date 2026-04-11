18b24befad0ba0f35933f5ccba0300027ea9bc22
tatertot456 <tjhoosh1@gmail.com>
2026-04-11 16:34:01 -0600

    Add all Razor pages

--- AniVault/Program.cs
+++ AniVault/Program.cs
@@ -1,28 +1,42 @@
-using AniVault.Components;
+using AniVault.Data;
+using AniVault.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Database
+builder.Services.AddDbContextFactory<AnimeTrackerContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// App Services
+builder.Services.AddScoped<AnimeService>();
+builder.Services.AddSingleton<ToastService>();
+
+// Jikan HTTP Client
+builder.Services.AddHttpClient<JikanService>(client =>
+{
+    client.BaseAddress = new Uri("https://api.jikan.moe/v4/");
+    client.DefaultRequestHeaders.Add("User-Agent", "AniVault/1.0");
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-
+app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
+app.MapRazorComponents<AniVault.Components.App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
